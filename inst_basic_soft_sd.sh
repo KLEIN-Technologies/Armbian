e5b3c651b8b9c334dbddab97f39f3a549610aa21
KLEIN-Technologies <klein.technologies.eg@gmail.com>
2025-03-16 05:37:43 +0200

    Update inst_basic_soft_sd.sh

--- inst_basic_soft_sd.sh
+++ inst_basic_soft_sd.sh
@@ -45,7 +45,7 @@ chmod a+r /etc/apt/keyrings/docker.asc
 
 echo \  
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \  
-  $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable" | \  
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \  
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
 
