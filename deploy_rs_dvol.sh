a7569d6f73dc4eab779683948e0e105788792e05
Samir Hebbo <122798260+KLEIN-Technologies@users.noreply.github.com>
2025-05-18 11:18:13 +0300

    Update deploy_rs_dvol.sh

--- deploy_rs_dvol.sh
+++ deploy_rs_dvol.sh
@@ -41,8 +41,8 @@ BACKUP_DEST="/00_SMB/Docker_Volumes"
 HOURLY_LIMIT=24
 COMPRESS_LEVEL=3
 
-TELEGRAM_TOKEN="7369801782:AAGltfab2_gIlP7l1aOFsxgXmalqUXz4FXw"
-TELEGRAM_CHAT_ID="8167593683"
+TELEGRAM_TOKEN="insert_token_here"
+TELEGRAM_CHAT_ID="insert_id_here"
 HOSTNAME=$(hostname)
 
 TELEGRAM_LOG="🚀 Starting Docker Volumes Backup...  
